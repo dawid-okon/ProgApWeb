7f242e21438bbed081b2483cbca64bea66de3a9e
Dawid Okoń <149314230+dawid-okon@users.noreply.github.com>
2024-05-26 13:35:16 +0200

    Delete form-route-todo-material directory

--- form-route-todo-material/index.jsx
+++
@@ -1,7 +0,0 @@
-// index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './style.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
